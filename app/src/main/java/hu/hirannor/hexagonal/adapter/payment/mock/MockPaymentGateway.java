e912db1d4e08e49451a0dc51c3bcc0313882b0e9
Mate Karolyi <mate_karolyi@outlook.com>
2025-09-13 10:26:35 +0200

    update svg

--- app/src/main/java/hu/hirannor/hexagonal/adapter/payment/mock/MockPaymentGateway.java
+++ app/src/main/java/hu/hirannor/hexagonal/adapter/payment/mock/MockPaymentGateway.java
@@ -1,7 +1,9 @@
 package hu.hirannor.hexagonal.adapter.payment.mock;
 
 import hu.hirannor.hexagonal.application.port.payment.PaymentGateway;
+import hu.hirannor.hexagonal.infrastructure.adapter.DrivenAdapter;
 
+@DrivenAdapter
 class MockPaymentGateway implements PaymentGateway {
     @Override
     public void pay() {
